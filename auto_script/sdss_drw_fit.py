0b7c007a971c99148ef1f865d75ab82ca7f7627b
ywx649999311 <wy73@drexel.edu>
2018-08-03 23:43:53 -0400

    update sdss_fit.py

--- auto_script/sdss_drw_fit.py
+++ auto_script/sdss_drw_fit.py
@@ -4,13 +4,23 @@ import pandas as pd
 import kali.carma
 import kali
 from joblib import Parallel, delayed
-import os, sys, glob
-sys.path.insert(0, '/home/mount/')
+from concurrent.futures import ProcessPoolExecutor
+import os, sys, glob, gc, random
+sys.path.insert(0, '/home/mount/lsst_cadence')
 from lsstlc import *  # derived LSST lightcurve sub-class
 
 
+# pre-defined for loading C. M. light curves
 names = ['mjd_u', 'mjd_g', 'mjd_r', 'mjd_i', 'mjd_z', 'ra', 'dec']
-cols = [0, 3, 6, 9, 12, 15, 16] 
+cols = [0, 3, 6, 9, 12, 15, 16]
+
+# carma task arguments
+nwalkers = 200
+nsteps = 1000
+
+bands = ['mjd_r', 'mjd_u']  # will loop over for each band in list
+shape = (nwalkers, nsteps)
+dtype = np.dtype([('LnPrior', np.float64, shape), ('LnLikelihood', np.float64, shape), ('LnPosterior', np.float64, shape), ('Chain[0]', np.float64, shape), ('Chain[1]', np.float64, shape), ('rootChain[0]', np.complex64, shape), ('rootChain[1]', np.complex64, shape), ('timescaleChain[0]', np.complex64, shape), ('timescaleChain[1]', np.complex64, shape)])
 
 
 def sdss_fit(lc, mjd_path):
@@ -23,58 +33,105 @@ def sdss_fit(lc, mjd_path):
     mjd = pd.read_csv(mjd_path, sep=' ', usecols=cols, names=names)
     ra = mjd.loc[0, 'ra']
     dec = mjd.loc[0, 'dec']
-    bands = ['mjd_r']  # will expand to all bands once enough cpu
-    best_param = []  # list to hold result from different bands
-    task = kali.carma.CARMATask(1, 0, nsteps=1000, nwalkers=200, nthreads=1)
+    best_param = []  # store best-fit params
+    mcmc = []  # store mcmc chain
+    task = kali.carma.CARMATask(1, 0, nsteps=nsteps, nwalkers=nwalkers, nthreads=1)
     
     for band in bands:
+
+        cad = (np.array(mjd[band])-mjd[band][0])*86400  # subtract first date
+        
+        # start fitting
         task.clear()
-        cad = (np.array(mjd[band])-mjd[band][0])*86400
         lc_down = lsstlc(ra, dec, cad, lc, fix_dt=True, band=band.split("_")[1])
         task.fit(lc_down)
+        
+        # fitted params and chains to array and pass back
         fit = list(task.bestTau)
         fit.append(band.split("_")[1])
         best_param.append(fit)
-    
+        mcmc.append(np.rec.array([task.LnPrior, task.LnLikelihood, task.LnPosterior, task.Chain[0], task.Chain[1], task.rootChain[0], task.rootChain[1], task.timescaleChain[0], task.timescaleChain[1]], dtype=dtype))
+
     df_p = pd.DataFrame(best_param, columns=['tau', 'sigma', 'filter'])
-    df_p['ra'] = ra
-    df_p['dec'] = dec
     df_p['file_name'] = os.path.split(mjd_path)[1]
     
-    return df_p
+    return (df_p, mcmc) 
+
+
+def read_lc(path_combo):
+    """Given path combo, return lc object."""
+
+    idx = path_combo[1]
+    path = os.path.join(path_combo[0], df_param.loc[idx, 'file'])
+    lc = extLC(path)
+    return lc
+
+
+def save(data):
+    """Parallelized task for saving fit data to file."""
+    
+    sd = data[1]
+    np.savez_compressed(data[0], fit=sd[0], chain=sd[1])
+
 
 if __name__ == '__main__':
     
-    lc_dir = sys.argv[1]
-    cad_dir = sys.argv[2]
-    input_param = sys.argv[3]
-    final_path = sys.argv[4]
+    lc_dir = sys.argv[1]  # Directory containning all simulated LCs
+    cad_dir = sys.argv[2]  # C. MacLeod LC path
+    input_param = sys.argv[3]  # csv containing the select mock LCs to fit among all
+    final_dir = sys.argv[4]  # The directory to save all output, one file per object
 
-    cad_files = glob.glob(os.path.join(cad_dir, '*'))
-    df_param = pd.read_csv(input_param)
+    if not os.path.exists(final_dir):
+        os.mkdir(final_dir)
+        print("Creating diretory: {}".format(final_dir))
 
-    array_ls = []  # master list to hold result for all simulated objects
-    for i in range(len(df_param)):
-        lc_path = os.path.join(lc_dir, df_param.loc[i, 'file'])
-        lc = extLC(lc_path)
+    cad_files = glob.glob(os.path.join(cad_dir, '*'))  # grab all C.M. lc files
+    df_param = pd.read_csv(input_param)  # load shortened object list
+    rt_id = 'c10_fit_{:.2f}_{:.2f}_{:d}'  # specify the saved record file standard
+    save_ls = []  # Hold results for every 4 objects in the first loop
 
-        # randomly select 100 cads to fit (not including n_obs < 50)
-        rd_idx = np.random.randint(len(cad_files), size=100)
-        s_100 = [cad_files[i] for i in rd_idx]
+    # random index to select from all lc
+    rd_idx = random.sample(range(len(cad_files)), k=100)
+    s_100 = [cad_files[i] for i in rd_idx]
+
+    for i in df_param.index[::4]:
+
+        # pack arguments into tuple
+        path_combos = [(lc_dir, i) for i in df_param.index[i:i+4]]
+
+        # using process pool to parallize file IO
+        with ProcessPoolExecutor(os.cpu_count()) as executor:
+            LCs = list(executor.map(read_lc, path_combos))
+
+        for j in range(len(LCs)):
+            
+            lc = LCs[j]  # take lc object out
+            rt_path = os.path.join(final_dir, rt_id.format(df_param.loc[i+j, 'tau'], df_param.loc[i+j, 'sigma'], i+j))  # file path for saved result
         
-        # start parallel code
-        best = Parallel(n_jobs=-1)(delayed(sdss_fit)(lc, path) for path in s_100)
-        
-        # put into one master frame and then to rec.array
-        dff = pd.concat(best, ignore_index=True)
-        dff['std_x'] = np.std(lc.x)
-        dff['std_y'] = np.std(lc.y)
-        dff['tauIn'] = df_param.loc[i, 'tau']
-        dff['sigmaIn'] = df_param.loc[i, 'sigma']
-        rec_arr = dff.to_records(index=False)
+            # start parallel code
+            try:
+                best = Parallel(n_jobs=-1)(delayed(sdss_fit)(lc, path) for path in s_100)
+            except Exception as inst:
+                print(inst)
+                print('Failed at object #{:d}'.format(i))
+                continue
+            
+            # put into one master frame and then to rec.array
+            df_ls = [best[i][0] for i in range(len(best))]
+            mcmc_ls = [best[i][1] for i in range(len(best))]
+            dff = pd.concat(df_ls, ignore_index=True)
+            dff['std_x'] = np.std(lc.x)
+            dff['std_y'] = np.std(lc.y)
+            dff['tauIn'] = df_param.loc[i, 'tau']
+            dff['sigmaIn'] = df_param.loc[i, 'sigma']
+            rec_dff = dff.to_records(index=False)
+            mcmc_arr = [mcmc_band for mcmc_path in mcmc_ls for mcmc_band in mcmc_path]
 
-        # append to master list
-        array_ls.append(rec_arr)
+            # put data need to be saved to a list
+            save_ls.append((rt_path, [rec_dff, mcmc_arr]))
 
-    # finally save all result to a file
-    np.save(final_path, array_ls)
+        with ProcessPoolExecutor(os.cpu_count()) as executor:
+            executor.map(save, save_ls)
+
+        # force garbage collection every 4 objects
+        gc.collect()
